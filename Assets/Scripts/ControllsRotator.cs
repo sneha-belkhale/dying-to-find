fe78c6c5432f064e3816d214096a813389b54cb2
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2019-07-12 14:52:45 -0700

    Added OVR controllers and simple controller rotation script

---
+++ Assets/Scripts/ControllsRotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllsRotator : MonoBehaviour
+{
+    private Quaternion targetRotation;
+    public float rotationSpeed = 0.80f;
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+    }
+    void Update()
+    {
+        float angle = Mathf.Abs(transform.eulerAngles.y - Camera.main.transform.eulerAngles.y);
+        if(angle > 90f)
+        {
+            targetRotation = Quaternion.Euler(0.0f, Camera.main.transform.eulerAngles.y, 0.0f);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+    }
+}
