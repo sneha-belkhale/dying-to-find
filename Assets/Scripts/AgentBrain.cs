6e6e8253c87edd294021b93ae180aae41dda1f34
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2019-10-11 15:48:43 -0700

    Agent look at, towers and fog

--- Assets/Scripts/AgentBrain.cs
+++ Assets/Scripts/AgentBrain.cs
@@ -4,20 +4,52 @@ using UnityEngine.AI;
 
 public class AgentBrain : MonoBehaviour
 {
-  public Transform goal;
+  [SerializeField]
+  private float handCollisionRadius = 0.5f;
   private NavMeshAgent m_Agent;
+  private Quaternion lastHeadRot;
+
+  public Transform goal;
+  public Transform headPos;
+  public Transform playerRightHand;
+  public Transform playerLeftHand;
+  public Transform playerHead;
 
   void Start()
   {
     m_Agent = GetComponent<NavMeshAgent>();
     m_Agent.destination = goal.position;
+    lastHeadRot = headPos.rotation;
   }
 
-  void Update()
+  void LateUpdate()
   {
     if (Vector3.Distance(transform.position, goal.position) < 2)
     {
       Destroy(gameObject);
     }
+
+    if((playerRightHand.position - headPos.position).magnitude < handCollisionRadius ||
+       (playerLeftHand.position - headPos.position).magnitude < handCollisionRadius)
+    {
+      lookAt();
+    } else {
+      lookBack();
+    }
+  }
+
+  private void lookAt()
+  {
+    Vector3 dir = (playerHead.position - headPos.position).normalized;
+    Quaternion newHeadRot = Quaternion.LookRotation(dir);
+
+    headPos.rotation = Quaternion.Lerp(lastHeadRot, newHeadRot, 2f * Time.deltaTime);
+    lastHeadRot = headPos.rotation;
+  }
+
+  private void lookBack()
+  {
+    headPos.rotation = Quaternion.Lerp(lastHeadRot, headPos.rotation, 3f * Time.deltaTime);
+    lastHeadRot = headPos.rotation;
   }
 }
